6fb3f62309953272477854d07820bf10bd455d10
Abhinav Pandey <abhinavpandey@Abhinavs-MacBook-Air.local>
2026-06-30 22:09:16 +0530

    feat: add reusable Skeleton loading component

---
+++ app/components/skeleton/skeleton.tsx
@@ -0,0 +1,20 @@
+import styles from "./skeleton.module.css";
+
+interface SkeletonProps {
+  width?: string;
+  height?: string;
+  className?: string;
+}
+
+export function Skeleton({
+  width = "100%",
+  height = "1rem",
+  className = "",
+}: SkeletonProps) {
+  return (
+    <div
+      className={`${styles.skeleton} ${className}`}
+      style={{ width, height }}
+    />
+  );
+}
