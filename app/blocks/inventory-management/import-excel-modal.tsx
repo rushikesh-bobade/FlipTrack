a768cfb7c5b36731cbae5051cd4b2635556e0019
Khushi1310-nayak <khushinayak127@gmail.com>
2026-07-04 19:06:28 +0530

    Change ts-expect-error to ts-ignore to fix CI build

--- app/blocks/inventory-management/import-excel-modal.tsx
+++ app/blocks/inventory-management/import-excel-modal.tsx
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react";
 import { useSubmit } from "react-router";
-// @ts-expect-error: read-excel-file/browser does not export types
+// @ts-ignore: read-excel-file/browser might not export types depending on the environment
 import { readSheet } from "read-excel-file/browser";
 import { toast } from "sonner";
 import { IconX, IconUpload } from "@tabler/icons-react";
