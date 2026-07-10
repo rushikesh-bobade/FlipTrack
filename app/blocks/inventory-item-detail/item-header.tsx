7b7514d26d62650b10bcbacf32395ccdb7bf0370
Sweta <254068510+sweta-126-ux@users.noreply.github.com>
2026-07-10 19:08:42 +0530

    fix stray character

--- app/blocks/inventory-item-detail/item-header.tsx
+++ app/blocks/inventory-item-detail/item-header.tsx
@@ -45,7 +45,7 @@ export function ItemHeader({ className, item }: Props) {
             <>
             <span>·</span>
             <span>{item.size}</span>
-        g </>
+          </>
   )}
 
   <span>·</span>
