ab010c76725704e0a9da08eb66505cf3ebfa805e
Janhavi Rathod <mdesktop262@gmail.com>
2026-07-12 14:23:24 +0530

    Update app/utils/supabase.client.ts
    Co-authored-by: Rushikesh Bobade <155564428+rushikesh-bobade@users.noreply.github.com>

--- app/utils/supabase.client.ts
+++ app/utils/supabase.client.ts
@@ -1,7 +1,22 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export function getSupabaseBrowserClient() {
-  const supabaseUrl =
+  let supabaseUrl: string | undefined;
+  let supabaseAnonKey: string | undefined;
+
+  try {
+    supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || process.env.SUPABASE_URL;
+    supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || process.env.SUPABASE_PUBLISHABLE_KEY;
+  } catch (e) {}
+
+  if (!supabaseUrl || !supabaseAnonKey) {
+    try {
+      // @ts-ignore
+      supabaseUrl = import.meta.env.VITE_NEXT_PUBLIC_SUPABASE_URL || import.meta.env.VITE_SUPABASE_URL;
+      // @ts-ignore
+      supabaseAnonKey = import.meta.env.VITE_NEXT_PUBLIC_SUPABASE_ANON_KEY || import.meta.env.VITE_SUPABASE_ANON_KEY;
+    } catch (e) {}
+  }
     import.meta.env.VITE_NEXT_PUBLIC_SUPABASE_URL ||
     process.env.NEXT_PUBLIC_SUPABASE_URL ||
     process.env.SUPABASE_URL!;
