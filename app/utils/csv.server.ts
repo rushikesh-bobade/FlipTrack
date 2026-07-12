61cded99747fa8a44ed22903b580a9376ef2c621
Shivangi Gupta <shivi14112007@gmail.com>
2026-07-12 16:14:14 +0530

    Update csv.server.ts

--- app/utils/csv.server.ts
+++ app/utils/csv.server.ts
@@ -1,13 +1,10 @@
 export function sanitizeCsvField(value: string | null | undefined): string {
   let sanitized = String(value ?? "");
-
   // Escape embedded double quotes
   sanitized = sanitized.replace(/"/g, '""');
-
   // Neutralize spreadsheet formula injection
-  if (/^[=+\-@\t\r]/.test(sanitized)) {
+  if (/^[=+\-@\t\r\n]/.test(sanitized)) {
     sanitized = `'${sanitized}`;
   }
-
   return sanitized;
 }
